d565620f447853c76857fad298eeb17f37eccfb9
reinaldo-pinto <reinaldo.souza@icarros.com.br>
2018-02-17 22:02:54 -0200

    Changes in script python, improvements.

--- scan-aws.py
+++ scan-aws.py
@@ -1,25 +1,25 @@
 import boto3
 
 ec2 = boto3.client('ec2')
-
+s3_service = boto3.resource('s3')
+bucket = "devops-files-"
 filtered = 'status'
 status = 'available'
 
-volume = ec2.describe_volumes(Filters=[{'Name': filtered, 'Values': [status]}])
+def lambda_handler(event, context):
 
-vol_list = []
-for i in volume['Volumes']:
-    data_list = {"ID": i['VolumeId'], "Size": str(i['Size'])+"G", "Status": i['State']}
-    vol_list.append(data_list)
+    volume = ec2.describe_volumes(Filters=[{'Name': filtered, 'Values': [status]}])
 
-result = {"Volumes": vol_list}
-print("\nVolumes without association the none resource:\n")
-print result
+    vol_list = []
+    for i in volume['Volumes']:
+        data_list = {"ID": i['VolumeId'], "Size": str(i['Size'])+"G", "Status": i['State']}
+        vol_list.append(data_list)
 
-arquivo = open('s3file.txt', 'w')
-arquivo.write(str(result))
-arquivo.close()
+    result = {"Volumes": vol_list}
+    print("\nVolumes without association the none resource:\n")
+    print(result)
 
-s3_service = boto3.resource('s3')
-bucket = "devops-files-"
-s3_service.Bucket(bucket).upload_file("s3file.txt", "s3file.txt")
+    arquivo = open('/tmp/s3file.txt', 'w')
+    arquivo.write(str(result))
+    arquivo.close()
+    s3_service.Bucket(bucket).upload_file("/tmp/s3file.txt", "s3file.txt")
